d954da87175895e3e4a521c19d7aaafc388a8475
Mustafif <mustafif0929@gmail.com>
2024-04-27 16:14:12 -0400

    updated install step

--- Formula/mufiz.rb
+++ Formula/mufiz.rb
@@ -20,8 +20,10 @@ class Mufiz < Formula
 
   def install
     # Extract the contents of the zip archive
-    system "unzip", "-q", cached_download.to_s, "-d", "#{buildpath}/mufiz"
-    bin.install "#{buildpath}/mufiz" # Adjust the binary name if necessary
+    system "unzip", "-q", cached_download.to_s, "-d", "#{buildpath}"
+
+    # Move the binary to /usr/local/bin
+    bin.install "#{buildpath}/mufiz"
   end
 
   test do
