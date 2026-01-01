de72b493431963cd67179769ebb65f5f75ce76b3
Mustafif <mustafif0929@gmail.com>
2026-01-01 01:30:01 -0500

    update stable to 0.10.0, experimental has not moved to 0.11.0 yet

--- Formula/mufiz.rb
+++ Formula/mufiz.rb
@@ -2,21 +2,21 @@ class Mufiz < Formula
   desc "Mufi Lang with Ziggyness"
   homepage "https://github.com/Mustafif/MufiZ"
 
-  version = "0.9.0"
+  version = "0.10.0"
 
-  url "https://github.com/Mustafif/MufiZ/releases/download/v0.9.0/mufiz_0.9.0_x86_64-linux.zip"
-  sha256 "237e801e5824ef2c5ab53a08136b02531bd2f8e8666ca41f3f0c47d35e96925a"
+  url "https://github.com/Mustafif/MufiZ/releases/download/v0.10.0/mufiz_0.10.0_x86_64-linux.zip"
+  sha256 "4cd5d0b505e6bbc5e9f9162843fcc83e594c059fbea8e2df1a8cb3822214168e"
   license "GPL-2.0-only"
 
   # URL and SHA256 hash for ARM Mac
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Mustafif/MufiZ/releases/download/v0.9.0/mufiz_0.9.0_aarch64-macos.zip"
-      sha256 "22db7700c70a5cbcf464439878190512ce48ad57b2a3603b78b7c1b69d3fd203"
+      url "https://github.com/Mustafif/MufiZ/releases/download/v0.10.0/mufiz_0.10.0_aarch64-macos.zip"
+      sha256 "b2769da0f52eb27cf0a5e67ab19b10f7a75006ec553b8304c4b95137b825d2cd"
     else
       # URL and SHA256 hash for Intel Mac
-      url "https://github.com/Mustafif/MufiZ/releases/download/v0.9.0/mufiz_0.9.0_x86_64-macos.zip"
-      sha256 "92f9efbeac2f746a393c95ec838d0d0e7cbc3a99782c8ce26ae46bbeb0475a78"
+      url "https://github.com/Mustafif/MufiZ/releases/download/v0.10.0/mufiz_0.10.0_x86_64-macos.zip"
+      sha256 "d9c4f33c5f2a5bee832e893965fcadd81ecbe08ee66d9178bae8a2b6c2a2db88"
     end
   end
 
